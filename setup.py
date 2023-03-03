800e18ff2d566d90f6158ea0a65a7fe32d0d3ca0
nopparuts <ak3.nopparut@gmail.com>
2023-03-03 15:16:40 +0700

    fix: classifiers and action test

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="thai-citizen-id",
-    version="0.1.3",
+    version="0.1.4",
     author="Nopparut Saelim",
     author_email="ak3.nopparut@gmail.com",
     description="Thailand citizen ID tools",
